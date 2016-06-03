750c0040eb4c9c49bf42a1fc881ee946d52f9eaf
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-06-03 14:31:57 +0800

    New Module "oclubs.objs"

---
+++ oclubs/objs/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+
+"""oclubs.shsid.org Objects."""
+
+from __future__ import absolute_import
+
+from oclubs.objs import user, club
+
+__all__ = ['user', 'club']
