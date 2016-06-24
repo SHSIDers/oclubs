519c353c7d4f87426a05d37672d097eb66069456
dlwocks <frank_smic@hotmail.com>
2016-06-24 16:08:24 +0800

    Add files via upload

--- database.py
+++ database.py
@@ -1,3 +1,7 @@
+import MySQLdb
+db = MySQLdb.connect(host="localhost", user="root", db="oclubs")
+cur = db.cursor()
+
 # cond: a tuple, like: "=","user_id",self.uid
 # for range: lower bound included, higher bound excluded
 
@@ -12,21 +16,65 @@ def cond_interpret(cond):
         return cond[1]+'>='+cond[2][0]+" AND " + cond[1]+'<'+cond[2][1]
 
 
-def fetch_onerow(table, conds, coldict):
+def fetch_onerow(table, conds, coldict, isand=True):
     cols = coldict.keys()
     st = ','.join(cols)
     fconds = []
     for cond in conds:
         fconds.append(cond_interpret(cond))
-    fconds = " AND ".join(fconds)
+    if isand:
+        fconds = " AND ".join(fconds)
+    else:
+        fconds = " OR ".join(fconds)
     cur.execute("SELECT %s FROM %s WHERE %s LIMIT 1" % (st, table, fconds))
-    ret = {}
-    ret.fromkeys(coldict.values())
     read = cur.fetchall()
     if len(read) == 0:
         raise RuntimeError
+    ret = {}
+    ret.fromkeys(coldict.values())
     i = 0
     for val in read[0]:
         ret[coldict[cols[i]]] = val
         i += 1
     return ret
+
+
+def fetch_oneblock(table, conds, col, isand=True):
+    fconds = []
+    for cond in conds:
+        fconds.append(cond_interpret(cond))
+    if isand:
+        fconds = " AND ".join(fconds)
+    else:
+        fconds = " OR ".join(fconds)
+    cur.execute("SELECT %s FROM %s WHERE %s LIMIT 1" % (col, table, fconds))
+    read = cur.fetchall()
+    if len(read) == 0:
+        raise RuntimeError
+    return read[0][0]
+
+
+def fetch_allrow(table, conds, coldict, isand=True):
+    cols = coldict.keys()
+    st = ','.join(cols)
+    fconds = []
+    for cond in conds:
+        fconds.append(cond_interpret(cond))
+    if isand:
+        fconds = " AND ".join(fconds)
+    else:
+        fconds = " OR ".join(fconds)
+    cur.execute("SELECT %s FROM %s WHERE %s" % (st, table, fconds))
+    reads = cur.fetchall()
+    if len(reads) == 0:
+        raise RuntimeError
+    rows = []
+    for read in reads:
+        row = {}
+        row.fromkeys(coldict.values)
+        i = 0
+        for val in read:
+            row[coldict[cols[i]]] = val
+            i += 1
+        rows.append(row)
+    return rows
