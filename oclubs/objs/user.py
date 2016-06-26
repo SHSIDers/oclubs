055b03cdd6d13de45b1d6850f557251dd5ba7c39
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-06-26 15:52:43 +0800

    user.py: update

--- oclubs/objs/user.py
+++ oclubs/objs/user.py
@@ -20,5 +20,16 @@ class User(object):
 
     def load_db(self):
         """Load data from db."""
-        # TODO
-        self.data = database()
+        self.data = database.fetch_onerow(
+            'user',
+            [('=', 'user_id', self.uid)],
+            {
+                'user_login_name': 'username',
+                'user_nick_name': 'nickname',
+                'user_type': 'type',
+                'user_grad_year': 'gradyear'
+            }
+        )
+
+    def attempt_login(self, pw):
+        
