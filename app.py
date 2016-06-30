34cb2b25d89f16662ba4a897d9f6c4341317a8b5
dlwocks <frank_smic@hotmail.com>
2016-06-30 10:46:14 +0800

    manual merge

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# coding=gbk
+
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 
@@ -5,14 +7,27 @@ from flask import (
     Flask, redirect, request, render_template, url_for
 )
 
+import traceback
+
 app = Flask(__name__)
+
+
+@app.errorhandler(Exception)
+def exception_handler(e):
+    '''Handle an exception and show the traceback'''
+    try:
+        return render_template('clublist.html',
+                               title='Here you come',
+                               loggedin=False)
+    except:
+        return traceback.format_exc()
 
 
 @app.route('/')
 def home():
     '''Homepage'''
     loggedin = False
-    return render_template('homepage.html',
+    return render_template('clublist.html',
                            title='Here you come',
                            loggedin=loggedin)
 
@@ -21,8 +36,8 @@ def home():
 def clublist():
     '''Club List'''
     loggedin = False
-    clubs = {{'name': 'art_club', 'photo': 'picture'},
-             {'name': 'photo_club', 'photo': 'picture'}}
+    clubs = [{'name': 'art_club', 'photo': 'picture'},
+             {'name': 'photo_club', 'photo': 'picture'}]
     return render_template('clublist.html',
                            title='Club List',
                            loggedin=loggedin,
