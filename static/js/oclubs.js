e80d7b7f8732e8e3cac5dfa00d8d4ff1a599c1e1
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-07-03 18:24:31 +0800

    newleader.html: fixes

--- static/js/oclubs.js
+++ static/js/oclubs.js
@@ -1,41 +1,47 @@
-(function ($) {
-	$(document).ready(function(){
-		$("#loginModal input[name='hasaccount'][value='1']").click();
-		$("#loginModal input[name='hasaccount']").change(function() {
-			switch ($("#loginModal input[name='hasaccount']:checked").val()) {
-				case '0':
-					$('#loginModal #loginform').hide();
-					$('#loginModal #registerform').show();
-					break;
-				default:
-					$('#loginModal #loginform').show();
-					$('#loginModal #registerform').hide();
-					break;
-			}
-		});
+( function( $ ) {
+	$( document )
+		.ready( function() {
+			$( "#loginModal input[name='hasaccount'][value='1']" )
+				.click();
+			$( "#loginModal input[name='hasaccount']" )
+				.change( function() {
+					switch ( $( "#loginModal input[name='hasaccount']:checked" )
+						.val() ) {
+						case '0':
+							$( '#loginModal #loginform' )
+								.hide();
+							$( '#loginModal #registerform' )
+								.show();
+							break;
+						default:
+							$( '#loginModal #loginform' )
+								.show();
+							$( '#loginModal #registerform' )
+								.hide();
+							break;
+					}
+				} );
 
-	    $(".clickable").click(function() {
-	        window.document.location = $(this).data("href");
-	    });
-	});
-})(jQuery);
+			$( ".clickable" )
+				.click( function() {
+					window.document.location = $( this )
+						.data( "href" );
+				} );
 
-function swing(x) {
-      x.className ='swing animated'
+			$( "#updatecheck" )
+				.click( function() {
+					var checked = $( '#leader_radio input[type=radio]:checked' );
+					if ( checked.size() > 0 ) {
+						$( ".modal .modal-body" )
+							.html( "<p>" + checked.val() + "</p>" );
+					} else {
+						$( ".modal .modal-body" )
+							.html( "<p>Please select one memeber as next club leader!</p>" );
+					}
+				} );
+		} );
+} )( jQuery );
+
+function swing( x ) {
+	x.className = 'swing animated';
 }
-
-function updatecheck(){
-    if($('input[type=radio]:checked').size() > 0) {
-        $(".modal modal-body").html("<p>" + document.getElementById('leader_radio').value + "</p>");
-    }else{
-        $(".modal modal-body").html("<p>Please select one memeber as next club leader!</p>");
-    }
-}
-
-$("#updatecheck").click(function() {
-	if($('input[type=radio]:checked').size() > 0) {
-        $(".modal .modal-body").html("<p>" + document.getElementById('leader_radio').value + "</p>");
-    }else{
-        $(".modal .modal-body").html("<p>Please select one memeber as next club leader!</p>");
-    }
-})
