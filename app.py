4b9b7a7e8c62e03555d627ee870bf63353e30140
Derril <IDTai@hotmail.com>
2016-07-09 11:16:57 +0800

    font color adjusted to background

--- app.py
+++ app.py
@@ -62,9 +62,11 @@ def clublist():
 def clubintro():
     '''Club Intro'''
     user = ''
+    intro = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     return render_template('clubintro.html',
                            title='Club Intro',
-                           user=user)
+                           user=user,
+                           intro=intro)
 
 
 @app.route('/allact')
