5e51b1c01fda595bed4d0b43b6e658685efaf1cf
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-07-14 17:22:22 +0800

    database.py: support for MOAR complicated queries

--- oclubs/access/database.py
+++ oclubs/access/database.py
@@ -6,7 +6,6 @@ from __future__ import unicode_literals
 
 from flask import g
 import MySQLdb
-import redis
 
 from oclubs.exceptions import NoRow
 
@@ -32,6 +31,57 @@ def ___parse_cond(cond):
         # (lo, hi]
         var, (lo, hi) = conds
         return __parse_cond('and', ('>=', var, lo), ('<', var, hi))
+
+
+def _parse_comp_cond(cond, forcelimit=None):
+    conddict = {
+        'join': [],  # list of tuple (type, table, [(var1, var2), ...])
+        'where': [],  # list of tuple (type, var, const)
+        'group': [],  # list of vars
+        'having': [],  # same as where
+        'order': [],  # tuple (var, is_asc)
+        'limit': None  # None or num or (lo, hi)
+    }
+    if isinstance(cond, list):
+        # this is a simple cond
+        conddict['where'] = cond
+    else:
+        conddict.update(cond)
+
+    if forcelimit:
+        conddict['limit'] = forcelimit
+
+    sql = []
+
+    for jointype, table, conds in conddict['JOIN']:
+        sql.append(jointype.upper + ' JOIN ' + table)
+        sql.append('ON ' + ' AND '.join(
+            [var1 + ' = ' + var2 for var1, var2 in conds]
+        ))
+
+    if conddict['where']:
+        sql.append('WHERE ' + _parse_cond(conddict['where']))
+
+    if conddict['group']:
+        sql.append('GROUP BY ' + ','.join(conddict['group']))
+
+    if conddict['having']:
+        sql.append('HAVING ' + _parse_cond(conddict['having']))
+
+    if conddict['order']:
+        sql.append('ORDER BY ' + ','.join([
+            var + ' ' + ('ASC' if is_asc else 'DESC')
+            for var, is_asc in conddict['order']
+        ]))
+
+    if conddict['limit']:
+        sql.append('LIMIT ' + (
+            '%d,%d' % conddict['limit']
+            if isinstance(conddict['limit'], tuple) else
+            str(conddict['limit'])
+        ))
+
+    return ' '.join(sql)
 
 
 def _encode(obj):
@@ -103,9 +153,9 @@ def done(commit=True):
 def fetch_onerow(table, conds, coldict):
     cols = coldict.keys()
     st = ','.join(cols)
-    conds = _parse_cond(conds)
+    conds = _parse_comp_cond(conds, forcelimit=1)
 
-    rows = _execute("SELECT %s FROM %s WHERE %s LIMIT 1;"
+    rows = _execute("SELECT %s FROM %s %s;"
                     % (st, table, conds))
     if not rows:
         raise NoRow
@@ -114,9 +164,9 @@ def fetch_onerow(table, conds, coldict):
 
 
 def fetch_oneentry(table, conds, col):
-    conds = _parse_cond(conds)
+    conds = _parse_comp_cond(conds, forcelimit=1)
 
-    rows = _execute("SELECT %s FROM %s WHERE %s LIMIT 1;"
+    rows = _execute("SELECT %s FROM %s %s;"
                     % (col, table, conds))
     if not rows:
         raise NoRow
@@ -125,9 +175,9 @@ def fetch_oneentry(table, conds, col):
 
 
 def fetch_onecol(table, conds, col):
-    conds = _parse_cond(conds)
+    conds = _parse_comp_cond(conds)
 
-    rows = _execute("SELECT %s FROM %s WHERE %s;"
+    rows = _execute("SELECT %s FROM %s %s;"
                     % (col, table, conds))
 
     return [val for val, in rows]
@@ -136,11 +186,20 @@ def fetch_onecol(table, conds, col):
 def fetch_multirow(table, conds, coldict):
     cols = coldict.keys()
     st = ','.join(cols)
-    conds = _parse_cond(conds)
+    conds = _parse_comp_cond(conds)
 
-    rows = _execute("SELECT %s FROM %s WHERE %s;" % (st, table, conds))
+    rows = _execute("SELECT %s FROM %s %s;" % (st, table, conds))
 
     return [_mk_multi_return(row, cols, coldict) for row in rows]
+
+
+def update_row(table, conds, update):
+    conds = _parse_comp_cond(conds)
+    setto = ["%s=%s" % (key, _encode(val)) for key, val in update.items()]
+    setto = ','.join(setto)
+
+    return _execute('UPDATE %s SET %s %s;' % (table, setto, conds),
+                    write=True)
 
 
 def insert_onerow(table, row):
@@ -148,13 +207,4 @@ def insert_onerow(table, row):
     values = ','.join([_encode(value) for value in row.values()])
 
     return _execute("INSERT INTO %s (%s) VALUES (%s);" % (table, keys, values),
-                    write=True)
-
-
-def update_row(table, conds, update):
-    conds = _parse_cond(conds)
-    setto = ["%s=%s" % (key, _encode(val)) for key, val in update.items()]
-    setto = ','.join(setto)
-
-    return _execute('UPDATE %s SET %s WHERE %s;' % (table, setto, conds),
                     write=True)
