2d585a6f8647e9420a7545a0460c1357f99cbd8d
Derril <IDTai@hotmail.com>
2016-07-17 13:51:51 +0800

    blueprint: modified

--- oclubs/actblueprint.py
+++ oclubs/actblueprint.py
@@ -13,6 +13,12 @@ import re
 actblueprint = Blueprint('actblueprint', __name__)
 
 
+def date_to_string(date):
+    date_str = str(date)
+    result = date_str[:4] + " - " + date_str[4:6] + " - " + date_str[6:8]
+    return result
+
+
 @actblueprint.route('/allact')
 def allactivities():
     '''All Activities'''
@@ -59,8 +65,7 @@ def clubactivities(club_info):
     for activity_obj in activities_obj:
         activity = {}
         activity['act_name'] = activity_obj.name
-        activity_date = str(activity_obj.date)
-        activity['time'] = activity_date[0:4] + " - " + activity_date[4:6] + " - " + activity_date[6:8]
+        activity['time'] = date_to_string(activity_obj.date)
         # activity['place']  = 
         activities.append(activity)
     return render_template('clubact.html',
@@ -128,63 +133,95 @@ def clubphoto(club_info):
 @actblueprint.route('/<club_info>/newact')
 def newact(club_info):
     '''Hosting New Activity'''
-    if('user_id' in session):
-        user_obj = oclubs.objs.User(session['user_id'])
-        user = user_obj.nickname
-    else:
-        user = ''
+    if 'user_id' not in session:
+        abort(401)
+    user_obj = oclubs.objs.User(session['user_id'])
     try:
         club_id = int(re.match(r'^\d+', club_info).group(0))
         club = oclubs.objs.Club(club_id)
     except:
         abort(404)
+    if user_obj.id != club.leader.id:
+        abort(403)
     return render_template('newact.html',
-                           title='New Activity',
-                           user=user)
+                           title='New Activity')
 
 
-@actblueprint.route('/act')
-def activity():
+@actblueprint.route('/<act_info>/introduction')
+def activity(act_info):
     '''Club Activity Page'''
-    user = ''
-    activity = {'club': 'Website Club', 'actname': 'Making Website',
-                'time': 'June 6, 2016', 'people': '20-30', 'intro': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'}
+    try:
+        act_id = int(re.match(r'^\d+', act_info).group(0))
+        act_obj = oclubs.objs.Activity(act_id)
+    except:
+        abort(404)
+    act = {}
+    act['club'] = act_obj.club.name
+    act['actname'] = act_obj.name
+    act['time'] = date_to_string(act_obj.date)
+    act['intro'] = act_obj.description
     return render_template('activity.html',
                            title=activity['actname'],
-                           user=user,
-                           activity=activity)
+                           activity=act)
 
 
-@actblueprint.route('/hongmei')
-def hongmei():
+@actblueprint.route('/<club_info>/hongmei')
+def hongmei(club_info):
     '''Check HongMei Status'''
-    user = ''
-    club = 'Website Club'
-    schedule = [{'date': 'June 6, 2016', 'members': [{'name': 'Derril', 'phone': '18918181818'},
-                                                     {'name': 'Frank', 'phone': '18918181818'},
-                                                     {'name': 'YiFei', 'phone': '18918181818'}]},
-                {'date': 'June 6, 2016', 'members': [{'name': 'Derril', 'phone': '18918181818'},
-                                                     {'name': 'Frank', 'phone': '18918181818'},
-                                                     {'name': 'YiFei', 'phone': '18918181818'}]},
-                {'date': 'June 6, 2016', 'members': [{'name': 'Derril', 'phone': '18918181818'},
-                                                     {'name': 'Frank', 'phone': '18918181818'},
-                                                     {'name': 'YiFei', 'phone': '18918181818'}]}]
+    if 'user_id' not in session:
+        abort(401)
+    user_obj = oclubs.objs.User(session['user_id'])
+    try:
+        club_id = int(re.match(r'^\d+', club_info).group(0))
+        club = oclubs.objs.Club(club_id)
+    except:
+        abort(404)
+    if user_obj.id != club.leader.id:
+        abort(403)
+    schedule = []
+    acts_obj = club.activities((False, False, False, True, False), (False, True))
+    for act_obj in acts_obj:
+        act = {}
+        act['date'] = act.date_to_string(act_obj.date)
+        members = []
+        members_list = act_obj.signup_list()
+        for member_list in members_list:
+            member = {}
+            member_obj = member_list.user
+            member['name'] = member_obj.nickname
+            member['phone'] = member_obj.phone
+            member['consentform'] = member_obj.consentform
+            members.append(member)
+        act['members'] = members
+        schedule.append(act)
     return render_template('hongmei.html',
                            title='HongMei',
-                           user=user,
                            club=club,
                            schedule=schedule)
 
 
-@actblueprint.route('/newhm')
-def newhm():
+@actblueprint.route('/<club_info>/newhm')
+def newhm(club_info):
     '''Input HongMei Plan'''
-    user = ''
-    club = 'Website Club'
+    if 'user_id' not in session:
+        abort(401)
+    user_obj = oclubs.objs.User(session['user_id'])
+    try:
+        club_id = int(re.match(r'^\d+', club_info).group(0))
+        club = oclubs.objs.Club(club_id)
+    except:
+        abort(404)
+    if user_obj.id != club.leader.id:
+        abort(403)
     return render_template('newhm.html',
                            title='HongMei Schedule',
-                           user=user,
-                           club=club)
+                           club=club.name)
+
+
+@actblueprint.route('/<club_info>/newhm/submit', methods=['POST'])
+def newhm_submit(club_info):
+    '''Input HongMei plan into databse'''
+    pass
 
 
 @actblueprint.route('/<act_info>/actstatus')
@@ -202,12 +239,18 @@ def actstatus(act_info):
     if user_obj.id != club.leader.id:
         abort(403)
     actname = act.name
-    date_str = str(act.date)
-    date = date_str[:4] + " - " + date_str[4:6] + " - " + date_str[6:8]
+    date = date_to_string(act.date)
     intro = act.descriptions
-    members = [{'name': 'Derril', 'email': 'lolol@outlook.com', 'phone': '18918181818'},
-               {'name': 'Frank', 'email': 'lolol@outlook.com', 'phone': '18918181818'},
-               {'name': 'YiFei', 'email': 'lolol@outlook.com', 'phone': '18918181818'}]
+    members = []
+    members_info = act.signup_list()
+    for member_info in members_info:
+        member = {}
+        member_obj = member_info['user']
+        member['name'] = member_obj.nickname
+        member['email'] = member_obj.email
+        member['phone'] = member_obj.phone
+        member['consentform'] = member_info['consentform']
+        members.append(member)
     members_num = 0
     for member in members:
         members_num += 1
@@ -237,8 +280,7 @@ def registerhm(club_info):
     for act_obj in acts_obj:
         act = {}
         act['id'] = act_obj.id
-        date = str(act_obj.date)
-        act['date'] = date[0:4] + " - " + date[4:6] + " - " + date[6:8]
+        act['date'] = date_to_string(act_obj.date)
         act['activity'] = act_obj.description
         schedule.append(act)
     return render_template('registerhm.html',
@@ -251,8 +293,6 @@ def registerhm(club_info):
 @actblueprint.route('/<club_info>/register_hongmei/submit')
 def registerhm_submit(club_info):
     '''Submit HongMei signup info to database'''
-    club_id = int(re.match(r'^\d+', club_info).group(0))
-    club = oclubs.objs.Club(club_id)
     user_obj = oclubs.objs.User(session['user_id'])
     register = request.form['register']
     for reg in register:
