33854b28fe3c228e892f51cb5deb52f2b418b846
Derril <IDTai@hotmail.com>
2016-07-22 22:06:39 +0800

    activity table adjusted

--- oclubs-tables.sql
+++ oclubs-tables.sql
@@ -50,7 +50,7 @@ CREATE TABLE IF NOT EXISTS activity (
 	act_id int unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	act_name varchar(255) NOT NULL,
 	act_club int NOT NULL, # Foreign key to club.club_id
-	act_desc int NOT NULL, # Foreign key to text.text_id
+	act_desc int, # Foreign key to text.text_id
 	act_date int unsigned NOT NULL,
 	act_time tinyint NOT NULL, # 0 = unknown, 1 = noon, 2 = afterschool, 3 = hongmei, 4 = others
 	act_location varchar(255) NOT NULL, # stores object in JSON
