c39938ad8fe447dfce7755e3783ccc089d916bbc
dlwocks <frank_smic@hotmail.com>
2016-07-24 10:39:47 +0800

    redis.py: fixed

--- oclubs/access/redis.py
+++ oclubs/access/redis.py
@@ -4,75 +4,70 @@
 from __future__ import absolute_import, unicode_literals
 
 from redis import Redis
-from Flask import g
+from flask import g
 import json
 
+
 r = Redis(host='localhost', port=6379, db=0)
-g.stuffList = {}
 
 
 def done():
-    for stuff in g.stuffList.values():
+    for stuff in g.redisStuffList.values():
         stuff.save()
 
 
 class RedisStuff(object):
-    def load(self):
+    def __init__(self):
+        exist = g.get('redisStuffList', None)
+        if not exist:
+            g.redisStuffList = {}
+        g.redisStuffList[self.key] = self
         self._initial = json.dumps(self)
+        super(RedisStuff, self).__init__()
+
+    def load(self):
         val = r.get(self.key)
         if val is None:
-            return ()
+            return ''
         try:
             ret = json.loads(val)
         except ValueError:
             r.delete(self.key)
-            return ()
+            return ''
         return ret
 
     def save(self, timeout=-1):
-        if self._initial != json.dumps(self):
-            r.set(self.key, json.dumps(self))
-            if timeout == -1:
-                r.persist(self.key)
-            else:
-                r.expire(self.key, timeout)
-            return True
-        return False
+        dumped = json.dumps(self)
+        changed = self._initial != dumped
+        if changed:
+            r.set(self.key, dumped)
+        if timeout == -1:
+            r.persist(self.key)
+        else:
+            r.expire(self.key, timeout)
+        return changed
 
 
 class RedisDict(dict, RedisStuff):
     def __init__(self, key):
         self.key = key
-        g.stuffList[self.key] = self
-        super(RedisDict, self).__init__(self.load())
+        dict.__init__(dict(self.load()))
+        RedisStuff.__init__(self)
 
 
 class RedisList(list, RedisStuff):
     def __init__(self, key):
-        self.key = key
-        g.stuffList[self.key] = self
-        super(RedisList, self).__init__(self.load())
+        list.__init__(list(self.load()))
+        RedisStuff.__init__(self)
 
 
-class RedisCache(str):
+class RedisCache(RedisStuff):
     def __init__(self, key):
         self.key = key
-        g.stuffList[self.key] = self
         super(RedisCache, self).__init__(self.load())
 
-    def load(self):
-        self._initial = str(self)
-        val = r.get(self.key)
-        if val is None:
-            return ''
-        return val
+    def set(self, value):
+        self._value = value
 
-    def save(self, timeout=-1):
-        if self._initial != str(self):
-            r.set(self.key, self)
-            if timeout == -1:
-                r.persist(self.key)
-            else:
-                r.expire(self.key, timeout)
-            return True
-        return False
+    def get(self):
+        return self._value
