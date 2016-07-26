f97bce2b4f5cae534b5e7eced37ec7ce36b53515
dlwocks <frank_smic@hotmail.com>
2016-07-26 23:13:23 +0800

    email.py: fix

--- oclubs/access/email.py
+++ oclubs/access/email.py
@@ -4,23 +4,24 @@
 
 """email module that sends email via SendGrid."""
 
+from __future__ import absolute_import, unicode_literals
+
 import sendgrid
 import os
-from sendgrid.helpers.mail import *
+from sendgrid.helpers.mail import Email, Content, Mail
+from oclubs.shared import get_secret
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+sg = sendgrid.SendGridAPIClient(apikey=get_secret('sendgrid_key'))
 from_email = Email("no-reply@oclubs.shsid.org","oClubs")
-
 
 def _send(to_email, subject, content):
     to_email = Email(to_email)
     mail = Mail(from_email, subject, to_email, content)
     try:
-        response = sg.client.mail.send.post(request_body=mail.get())
-    except:
+        sg.client.mail.send.post(request_body=mail.get())
+    except HTTPError:
         print "Error occured while sending email" #FIXME
 
-# Receives to_emails parameter either in string(single email) or in list(multiple emails)
 def send(to_emails, subject, content): 
     content = Content("text/plain", content)
     if isinstance(to_emails, str):
@@ -28,4 +29,5 @@ def send(to_emails, subject, content):
     elif isinstance(to_emails, list):
         for email in to_emails:
             _send(email, subject, content)
-    raise RuntimeError("invalid to_emails parameter")
+    else:
+        raise RuntimeError("invalid to_emails parameter")
