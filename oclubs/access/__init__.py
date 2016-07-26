d134b4fed9cb0874c2d82f54f3d14d2ef6ac9aae
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-07-26 22:36:22 +0800

    Fixed email

--- oclubs/access/__init__.py
+++ oclubs/access/__init__.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 
 from oclubs.access import database
 from oclubs.access import elasticsearch
+from oclubs.access import email
 from oclubs.access import redis
 
 
@@ -16,4 +17,4 @@ def done(commit=True):
     redis.done(commit)
 
 
-__all__ = ['database', 'elasticsearch', 'redis', 'done']
+__all__ = ['database', 'elasticsearch', 'email', 'redis', 'done']
