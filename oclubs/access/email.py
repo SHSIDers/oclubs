4881a0f68bed14f4fb9bbfee39afe4311edeb6f9
dlwocks <frank_smic@hotmail.com>
2016-07-28 13:03:28 +0800

    email.py: fixed

--- oclubs/access/email.py
+++ oclubs/access/email.py
@@ -7,27 +7,23 @@
 from __future__ import absolute_import, unicode_literals
 
 import sendgrid
-import os
 from sendgrid.helpers.mail import Email, Content, Mail
 from oclubs.shared import get_secret
 
 sg = sendgrid.SendGridAPIClient(apikey=get_secret('sendgrid_key'))
-from_email = Email("no-reply@oclubs.shsid.org","oClubs")
+from_email = Email("no-reply@oclubs.shsid.org", "oClubs")
+
 
 def _send(to_email, subject, content):
     to_email = Email(to_email)
     mail = Mail(from_email, subject, to_email, content)
-    try:
-        sg.client.mail.send.post(request_body=mail.get())
-    except HTTPError:
-        print "Error occured while sending email" #FIXME
+    sg.client.mail.send.post(request_body=mail.get())
 
-def send(to_emails, subject, content): 
+
+def send(to_emails, subject, content):
     content = Content("text/plain", content)
     if isinstance(to_emails, str):
         _send(to_emails, subject, content)
     elif isinstance(to_emails, list):
         for email in to_emails:
             _send(email, subject, content)
-    else:
-        raise RuntimeError("invalid to_emails parameter")
