b9ab3df7fc542ea003d417174d912b341b9ed23e
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-08-03 22:18:51 +0800

    Conditional emailing

--- oclubs/access/email.py
+++ oclubs/access/email.py
@@ -5,16 +5,26 @@
 from __future__ import absolute_import, unicode_literals
 
 from envelopes import Envelope, SMTP
+import sendgrid
+from sendgrid.helpers.mail import Email, Content, Mail
+
+from oclubs.access import get_secret
 
 from_email = ('no-reply@oclubs.shsid.org', 'oClubs')
 
 
 def send(to_email, subject, content):
-    conn = SMTP('127.0.0.1', 25)
-    mail = Envelope(
-        to_addr=to_email,
-        from_addr=from_email,
-        subject=subject,
-        text_body=content
-    )
-    conn.send(mail)
+    if to_email[0].strip().endswith('@gmail.com'):
+        sg = sendgrid.SendGridAPIClient(apikey=get_secret('sendgrid_key'))
+        content = Content('text/plain', content)
+        mail = Mail(Email(*from_email), subject, Email(*to_email), content)
+        sg.client.mail.send.post(request_body=mail.get())
+    else:
+        conn = SMTP('127.0.0.1', 25)
+        mail = Envelope(
+            to_addr=to_email,
+            from_addr=from_email,
+            subject=subject,
+            text_body=content
+        )
+        conn.send(mail)
