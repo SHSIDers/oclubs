3a8e757c2f987021d643a996d07e4160249c36a5
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-08-03 15:10:08 +0800

    email: python's smtp is not thread-safe

--- oclubs/access/email.py
+++ oclubs/access/email.py
@@ -6,11 +6,11 @@ from __future__ import absolute_import, unicode_literals
 
 from envelopes import Envelope, SMTP
 
-conn = SMTP('127.0.0.1', 25)
 from_email = ('no-reply@oclubs.shsid.org', 'oClubs')
 
 
 def send(to_email, subject, content):
+    conn = SMTP('127.0.0.1', 25)
     mail = Envelope(
         to_addr=to_email,
         from_addr=from_email,
