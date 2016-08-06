32dbc7f53301be2b35ee2dfb43359912417ef515
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-08-06 21:17:56 +0800

    homepage: better code

--- oclubs/app.py
+++ oclubs/app.py
@@ -227,30 +227,27 @@ def _search_gettext(obj, name):
 def homepage():
     '''Homepage'''
     top_pic = []
-    acts = Activity.get_activities_conditions(require_photos=True, limit=(0, 6))[1]
     sizes = [3, 6, 3, 6, 3, 3]
-    size_count = 0
-    for act in acts:
-        if len(top_pic) == 6:
-            break
-        each = {}
-        each['size'] = sizes[size_count]
-        size_count += 1
-        each['picture'] = act.pictures[0]
-        each['actname'] = act.name
-        each['content'] = act.description.formatted
-        each['link'] = url_for('actblueprint.activity', activity=act.callsign)
-        top_pic.append(each)
-    if size_count < 5:
-        for miss in range(6 - size_count):
-            fill = {}
-            fill['size'] = sizes[size_count]
-            size_count += 1
-            fill['picture'] = Upload(-101)
-            fill['actname'] = 'Default'
-            fill['content'] = 'Oops, we don\'t have much picture'
-            fill['link'] = '#'
-            top_pic.append(fill)
+    acts = Activity.get_activities_conditions(
+        require_photos=True, limit=len(sizes))[1]
+
+    top_pic.extend([{
+        'picture': act.pictures[0],
+        'actname': act.name,
+        'content': act.description.formatted,
+        'link': url_for('actblueprint.activity', activity=act.callsign)
+    } for act in acts])
+
+    top_pic.extend([{
+        'picture': Upload(-101),
+        'actname': 'Default',
+        'content': 'Oops, we don\'t have much picture',
+        'link': '#'
+    } for _ in range(len(sizes) - len(top_pic))])
+
+    for pic, size in zip(top_pic, sizes):
+        pic['size'] = size
+
     ex_clubs = Club.excellentclubs(3)
     pic_acts = Activity.get_activities_conditions(require_photos=True,
                                                   limit=(0, 3))
