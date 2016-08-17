0fd566bd439105d348bae4855a0976e4955e04c0
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-08-18 04:19:26 +0800

    Using gulp's building

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,24 +1,16 @@
 var gulp = require('gulp');
 var compass = require('gulp-compass');
 var autoprefixer = require('gulp-autoprefixer');
-
-
-var input = './oclubs/static/sass/*.scss';
-var output = './oclubs/static/css';
-var style = 'compressed';
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
 
 var compassOptions = {
-	sass: './oclubs/static/sass',
-	css: './oclubs/static/css',
-	logging: true,
-	comments: false,
-	style: style
-};
-
-var sassOptions = {
-	errLogToConsole: true,
-	outputStyle: style,
-	lineComments: false,
+	sass: './oclubs/static-dev/sass',
+	css: './oclubs/static-dev/css',
+	images: './oclubs/static/images',
 };
 
 var autoprefixerOptions = {
@@ -39,20 +31,52 @@ var autoprefixerOptions = {
 	]
 };
 
-gulp.task('sass', function () {
+var plumberErrorHandler = {
+	errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }
+};
+
+gulp.task('styles', function () {
 	return gulp
-		.src(input)
+		.src('./oclubs/static-dev/sass/*.scss')
+		.pipe(plumber(plumberErrorHandler))
 		.pipe(compass(compassOptions))
-		.pipe(autoprefixer(autoprefixerOptions))
-		.pipe(gulp.dest(output));
+		.pipe(autoprefixer(autoprefixerOptionsZZ))
+		.pipe(gulp.dest('./oclubs/static-dev/css'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('./oclubs/static/css'));
+});
+
+gulp.task('scripts', function() {
+	return gulp
+		.src('./oclubs/static-dev/js/*.js')
+		.pipe(plumber(plumberErrorHandler))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(uglify())
+		.pipe(gulp.dest('./oclubs/static/js'));
+});
+
+gulp.task('images', function() {
+	return gulp
+		.src('./oclubs/static-dev/images/*')
+		.pipe(plumber(plumberErrorHandler))
+		.pipe(imagemin())
+		.pipe(gulp.dest('./oclubs/static/images'));
 });
 
 gulp.task('watch', function() {
-	return gulp
-		.watch(input, ['sass'])
-		.on('change', function(event) {
-			console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-		});
+	var changeevent = function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	};
+	gulp.watch('./oclubs/static-dev/sass/*.scss', ['styles'])
+		.on('change', changeevent);
+	gulp.watch('./oclubs/static-dev/js/*.js', ['scripts'])
+		.on('change', changeevent);
+	gulp.watch('./oclubs/static-dev/images/*', ['images'])
+		.on('change', changeevent);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
