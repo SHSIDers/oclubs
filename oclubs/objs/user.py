9f18d5ee60219f513812589fa95cd4d654e12ba2
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-08-23 17:08:41 +0800

    Revert "club invitation by request"
    This reverts commit efd7e25118238ab6e752b36190a98c040b70b8c4.

--- oclubs/objs/user.py
+++ oclubs/objs/user.py
@@ -79,7 +79,7 @@ class User(BaseObject, UserMixin):
         address = self.email or 'root@localhost'
         email.send((address, self.passportname), title, contents)
 
-    def notify_user(self, contents, invite=0, club=None):
+    def notify_user(self, contents):
         from oclubs.objs.activity import date_int
         database.insert_row(
             'notification',
@@ -87,9 +87,7 @@ class User(BaseObject, UserMixin):
                 'notification_user': self.id,
                 'notification_text': contents,
                 'notification_isread': False,
-                'notification_date': date_int(date.today()),
-                'notification_invitestatus': invite,
-                'notification_inviteclub': club
+                'notification_date': date_int(date.today())
             }
         )
 
@@ -102,12 +100,9 @@ class User(BaseObject, UserMixin):
             database.fetch_multirow,
             'notification',
             {
-                'notification_id': 'id',
                 'notification_text': 'text',
                 'notification_isread': 'isread',
-                'notification_date': 'date',
-                'notification_invitestatus': 'invitestatus',
-                'notification_inviteclub': 'inviteclub'
+                'notification_date': 'date'
             },
             {
                 'where': [('=', 'notification_user', self.id)],
