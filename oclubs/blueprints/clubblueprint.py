7ef773f8ec7551542ef2a6881885ca2ed2f04723
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-09-12 15:15:51 +0800

    clubblueprint: from __future__ import division

--- oclubs/blueprints/clubblueprint.py
+++ oclubs/blueprints/clubblueprint.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 #
 
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals, division
 
 from datetime import date
 
