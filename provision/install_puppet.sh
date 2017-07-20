5886cc7b8dd1bc62419d44d95d60f8b3c980ffad
Raymond <itsuncheng2000@gmail.com>
2017-07-20 08:02:58 +0800

    Revert "install_puppet.sh: Install puppet from yum.puppetlabs.com"
    This reverts commit 166f0307cb559908c62524fc3a22b594fa5cffe1.

--- provision/install_puppet.sh
+++ provision/install_puppet.sh
@@ -1,6 +1,2 @@
 #! /bin/sh
-if ! which puppet &> /dev/null; then
-    yum install -y epel-release
-    rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-    yum install -y puppet
-fi
+which puppet &> /dev/null || ( yum install -y epel-release; yum install -y puppet )
