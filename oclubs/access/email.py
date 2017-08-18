6a7b98a9096d5ae3813d1d532827ba8cc3805731
Raymond <itsuncheng2000@gmail.com>
2017-08-18 18:07:47 +0800

    email.py: Deleted Sendgrid
    We do not need Sendgrid anymore. We can just send emails directly.

--- oclubs/access/email.py
+++ oclubs/access/email.py
@@ -13,10 +13,7 @@ from __future__ import absolute_import, unicode_literals
 import traceback
 
 from envelopes import Envelope, SMTP
-import sendgrid
-from sendgrid.helpers.mail import Email, Content, Mail
 
-from oclubs.access.secrets import get_secret
 from oclubs.access.delay import delayed_func
 
 from_email = ('no-reply@oclubs.shs.cn', 'oClubs')
@@ -31,24 +28,15 @@ def send(to_email, subject, content):
     :param basestring subject: email subject
     :param basestring content: email content
     """
-    if not get_secret('sendgrid_key'):
-        # This is a test machine
-        return
 
     try:
-        if to_email[0].endswith('@gmail.com'):
-            sg = sendgrid.SendGridAPIClient(apikey=get_secret('sendgrid_key'))
-            content = Content('text/plain', content)
-            mail = Mail(Email(*from_email), subject, Email(to_email[0]), content)
-            sg.client.mail.send.post(request_body=mail.get())
-        else:
-            conn = SMTP('127.0.0.1', 25)
-            mail = Envelope(
-                to_addr=to_email[0],
-                from_addr=from_email,
-                subject=subject,
-                text_body=content
-            )
-            conn.send(mail)
+        conn = SMTP('127.0.0.1', 25)
+        mail = Envelope(
+            to_addr=to_email[0],
+            from_addr=from_email,
+            subject=subject,
+            text_body=content
+        )
+        conn.send(mail)
     except Exception:
         traceback.print_exc()
